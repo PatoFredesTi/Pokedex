736d17c623deffc2441f7d76875b2132a4b21bd7
Duck2693 <patricio.fredes.g@gmail.com>
2021-04-15 17:12:39 -0400

    Agregar tabla, form, y js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.querySelector("#registrar-btn").addEventListener("click", ()=>{
+    let nombre = document.querySelector("#nombre-txt").value;
+    
+    alert("Una chica caliente quiere conocerte " + nombre);
+});
