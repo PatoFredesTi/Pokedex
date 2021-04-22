a91e3ab4d29a76b8a93da6b1070f50e9f0bb28f7
Duck2693 <patricio.fredes.g@gmail.com>
2021-04-22 17:20:12 -0400

    Manipular el dom y agregar el tinymc

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
+
+tinymce.init({
+    selector: '#descripcion-txt',
+    height: 200,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+  
 document.querySelector("#registrar-btn").addEventListener("click", ()=>{
+    //value es para obtener el valor de los input de texto
     let nombre = document.querySelector("#nombre-txt").value;
-    
-    alert("Hola Mundo!" + nombre);
+    //esto lo saque de la pagina tinymce, es para obtener lo escrito
+    let descripcion = tinymce.get("descripcion-txt").getContent(); 
+    //checked indica si el radiobutton esta seleccionado
+    let legendario = document.querySelector("#legendario-si").checked;   
+    //El tipo se obtiene igual que los input
+    let tipo = document.querySelector("#tipo-select").value;
+    let evolucion = document.querySelector("#evolucion-si").checked;
+    console.log("Hola Mundo!", nombre, descripcion, legendario, evolucion, tipo);
 });
